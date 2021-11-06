7b91ff5644b783e646708c9c4f80e07bb7f17ea1
maginawin <maginawin@163.com>
2021-11-06 14:43:07 +0800

    Fix ZV9020A, external ZG9100A_S

--- drivers/ZG9100A_S/device.js
+++ drivers/ZG9100A_S/device.js
@@ -1,7 +1,10 @@
 'use strict'
 
 const { ZigBeeDevice } = require('homey-zigbeedriver')
-const { CLUSTER } = require('zigbee-clusters')
+const { CLUSTER, Cluster } = require('zigbee-clusters')
+
+const SrBasicCluster = require('../../lib/SrBasicCluster')
+Cluster.addCluster(SrBasicCluster)
 
 class MyLight extends ZigBeeDevice {
 
@@ -76,8 +79,57 @@ class MyLight extends ZigBeeDevice {
       })
     }
 
+    this._readExternalSwitchType()
   }
 
+  async onSettings ({ oldSettings, newSettings, changedKeys }) {
+
+    if (typeof newSettings['external_switch_type'] === 'string') {
+      const newType = newSettings['external_switch_type']
+      this.log('newType ' + newType)
+
+      let value = 0x00
+
+      if (newType === 'push_button') {
+        value = 0x00
+      } else if (newType === 'normal_on_off') {
+        value = 0x01
+      } else if (newType === 'three_channels') {
+        value = 0x02
+      }
+
+      this._basicCluster().writeAttributes({
+        externalSwitchType: value,
+      }).then(() => {
+        this.log('write new type success')
+      }).catch(this.error)
+    }
+  }
+
+  _readExternalSwitchType () {
+
+    this._basicCluster().readAttributes('externalSwitchType').then(value => {
+      this.log(`read externalSwitchType`, value)
+
+      const typeValue = value['externalSwitchType']
+      let type = 'push_button'
+      if (typeValue === 0) {
+        type = 'push_button'
+      } else if (typeValue === 1) {
+        type = 'normal_on_off'
+      } else if (typeValue === 2) {
+        type = 'three_channels'
+      }
+      this.setSettings({
+        'external_switch_type': type,
+      }).catch(this.error)
+
+    }).catch(this.error)
+  }
+
+  _basicCluster () {
+    return this.zclNode.endpoints[1].clusters.basic
+  }
 }
 
 module.exports = MyLight
