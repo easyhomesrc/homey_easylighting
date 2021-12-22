d8e2b4baab9085c39d68c85f415fa8c08e99a007
maginawin <maginawin@163.com>
2021-12-22 16:37:26 +0800

    Add external switch type settings for ZG9101SAC_HP.

--- drivers/ZG9101SAC_HP/device.js
+++ drivers/ZG9101SAC_HP/device.js
@@ -5,9 +5,18 @@ const SrSceneCluster = require('../../lib/SrSceneCluster')
 
 const { CLUSTER, Cluster } = require('zigbee-clusters')
 
+const SrBasicCluster = require('../../lib/SrBasicCluster')
+Cluster.addCluster(SrBasicCluster)
+
 Cluster.addCluster(SrSceneCluster)
 
 class DimLight extends SrZigbeeLight {
+
+  async onNodeInit ({ zclNode, node }) {
+    super.onNodeInit({ zclNode, node })
+
+    this._readExternalSwitchType()
+  }
 
   async levelStepRunListener (args, state) {
 
@@ -92,6 +101,55 @@ class DimLight extends SrZigbeeLight {
     const scenesCluster = this.getClusterEndpoint(CLUSTER.SCENES)
     if (scenesCluster === null) throw new Error('missing_scenes_cluster')
     return this.zclNode.endpoints[scenesCluster].clusters.scenes
+  }
+
+  async onSettings ({ oldSettings, newSettings, changedKeys }) {
+
+    if (typeof newSettings['external_switch_type'] === 'string') {
+      const newType = newSettings['external_switch_type']
+      this.log('newType ' + newType)
+
+      let value = 0x00
+
+      if (newType === 'push_button') {
+        value = 0x00
+      } else if (newType === 'normal_on_off') {
+        value = 0x01
+      } else if (newType === 'three_channels') {
+        value = 0x02
+      }
+
+      this._basicCluster().writeAttributes({
+        externalSwitchType: value,
+      }).then(() => {
+        this.log('write new type success')
+      }).catch(this.error)
+    }
+  }
+
+  _readExternalSwitchType () {
+
+    this._basicCluster().readAttributes('externalSwitchType').then(value => {
+      this.log(`read externalSwitchType`, value)
+
+      const typeValue = value['externalSwitchType']
+      let type = 'push_button'
+      if (typeValue === 0) {
+        type = 'push_button'
+      } else if (typeValue === 1) {
+        type = 'normal_on_off'
+      } else if (typeValue === 2) {
+        type = 'three_channels'
+      }
+      this.setSettings({
+        'external_switch_type': type,
+      }).catch(this.error)
+
+    }).catch(this.error)
+  }
+
+  _basicCluster () {
+    return this.zclNode.endpoints[1].clusters.basic
   }
 
 }
